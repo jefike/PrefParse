d6c28d8d0c246c4b5769adcf315e03e686c9841a
John Fike <jefike@gmail.com>
2017-06-27 13:56:11 -0400

    Updated transitive closure code to use proper notion of reachability in DFS.

--- trans_close.c
+++ trans_close.c
@@ -14,24 +14,30 @@ void print_tc(int tc[V][V]){
     printf("\n");
   }
 }
-void DFS(int g[][V], int visited[], int v){
+
+void DFS2(int g[][V], int visited[], int v){
   visited[v] = 1;
   
   for(int i = 0; i < V; ++i){
     if(i != v && g[v][i] == 1 && visited[i] == 0)
-      DFS(g,visited, i);
+      DFS2(g,visited, i);
+  }
+}
+
+void DFS(int g[][V], int v_track[],int visited[], int v){
+  v_track[v] = 1;
+  
+  for(int i = 0; i < V; ++i){
+    if(i != v && g[v][i] == 1 && visited[i] == 0)
+      DFS2(g,visited, i);
   }
 }
 
 void trans_close(int g[][V], int t_c[][V]){
+  int v_track[V][V];
+  memset(v_track,0,sizeof(int)*(V*V));
   for( int i = 0; i < V; ++i){
-    DFS(g,t_c[i],i); 
-  }
-  for( int i = 0; i < V; ++i){
-    for( int j = 0; j < V; ++j){
-      if(i == j)
-	t_c[i][j] = 0;
-    }
+    DFS(g,v_track[i], t_c[i],i); 
   }
 }
     
